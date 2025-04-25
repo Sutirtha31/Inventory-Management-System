ce16e97e6b88039a44db532e549b413795232d61
Sutirtha31 <132227096+Sutirtha31@users.noreply.github.com>
2025-04-26 04:00:37 +0530

    Update app.ts

--- server/src/app.ts
+++ server/src/app.ts
@@ -10,7 +10,7 @@ const app: Application = express();
 app.use(express.json());
 app.use(morgan('dev'));
 app.use(cors({
-  origin: ['http://localhost:5173', 'https://inventory-navy.vercel.app'],
+  origin: ['http://localhost:5173'],
 }));
 
 // Root route just to test the server
